acb7baf2895eb4887f8c3574d1dab1409f5ba7b5
Botuca <botucacontato@gmail.com>
2022-07-26 23:38:39 -0300

    add connected-circles

---
+++ connected-circles.js
@@ -0,0 +1,106 @@
+const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const math = require("canvas-sketch-util/math");
+
+const settings = {
+  dimensions: [2048, 2048],
+  animate: true,
+  fps: 50,
+};
+
+const sketch = ({ context, width, height }) => {
+  const agents = [];
+
+  for (let i = 0; i < 50; i++) {
+    agents.push(new Agent(random.range(0, width), random.range(0, height)));
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "#080808";
+    context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 400) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 400, 12, 2);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.strokeStyle = "#7E1F86";
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
+    agents.forEach((agent) => {
+      agent.update();
+      agent.draw(context);
+      agent.bounce(width, height);
+      // agent.wrap(width, height);
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-10, 10), random.range(-2, 2));
+    this.radius = random.range(20, 50);
+  }
+
+  wrap(width, height) {
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.x > width) this.pos.x = 0;
+
+    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.y > height) this.pos.y = 0;
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
+  draw(context) {
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
+    context.beginPath();
+    context.strokeStyle = "#EEB300";
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.restore();
+  }
+}
